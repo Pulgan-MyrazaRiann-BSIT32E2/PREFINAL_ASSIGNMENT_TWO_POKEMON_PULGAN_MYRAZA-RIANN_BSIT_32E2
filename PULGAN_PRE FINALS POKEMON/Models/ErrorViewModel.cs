96b315f02b98ba9e1a36c78c2a3d9d4a04ac0566
Pulgan-MyrazaRiann-BSIT32E2 <81751499+Pulgan-MyrazaRiann-BSIT32E2@users.noreply.github.com>
2024-06-30 20:02:26 +0800

    Delete PULGAN_PRE FINALS POKEMON directory

--- PULGAN_PRE FINALS POKEMON/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PULGAN_PRE_FINALS_POKEMON.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
