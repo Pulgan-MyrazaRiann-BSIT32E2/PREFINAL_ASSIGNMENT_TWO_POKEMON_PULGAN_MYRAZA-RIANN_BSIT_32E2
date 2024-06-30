2ad18e473eb7724e13106aec49e57748ae0e9ce2
Pulgan-MyrazaRiann-BSIT32E2 <81751499+Pulgan-MyrazaRiann-BSIT32E2@users.noreply.github.com>
2024-06-30 17:53:37 +0800

    Add files via upload

---
+++ Pulgan_Pokemon/PULGAN_PRE FINALS POKEMON/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PULGAN_PRE_FINALS_POKEMON.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
