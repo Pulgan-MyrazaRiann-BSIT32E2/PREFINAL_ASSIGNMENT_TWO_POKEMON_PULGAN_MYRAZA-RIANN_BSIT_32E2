91390efcbad8e0d42584f3e6cb82f0f1215ec49c
Pulgan-MyrazaRiann-BSIT32E2 <81751499+Pulgan-MyrazaRiann-BSIT32E2@users.noreply.github.com>
2024-06-30 20:06:23 +0800

    Add files via upload

---
+++ Pulgan_Pokemon/PULGAN_PRE FINALS POKEMON/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PULGAN_PRE_FINALS_POKEMON.Controllers
+{
+    public class PokemonController : Controller
+    {
+
+            private readonly HttpClient _httpClient;
+
+            public PokemonController()
+            {
+                _httpClient = new HttpClient();
+            }
+
+            public async Task<IActionResult> Index(string name = "jigglypuff")
+            {
+                var pokemon = await GetPokemon(name);
+                return View(pokemon);
+            }
+
+            private async Task<Pokemon> GetPokemon(string name)
+            {
+                var response = await _httpClient.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{name}");
+                return JsonConvert.DeserializeObject<Pokemon>(response);
+            }
+        }
+    }
